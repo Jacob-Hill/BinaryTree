c13a2f62753e55bace76f41fcb772ed54602e671
Jacob Hill <hillj@stpaulsschool.org.uk>
2021-02-25 15:41:11 +0000

    Added a search function and a remove function, and fixed the add function such that the new nodes added themselves as children to their parents.

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -8,14 +8,13 @@ namespace BinaryTree
     {
         private class TreeNode
         {
-            TreeNode ParentNode { get; set; }
-            public TreeNode[] ChildrenNodes = new TreeNode[2];
+            public TreeNode ParentNode { get; set; }
+            public TreeNode[] ChildrenNodes = { null, null };
             public string Data { get; set; }
             public TreeNode(string data, TreeNode parent)
             {
                 Data = data;
                 ParentNode = parent;
-                ChildrenNodes = [null, null];
             }
         }
 
@@ -26,53 +25,57 @@ namespace BinaryTree
             root = new TreeNode(data, null);
         }
 
-        void AddChild(string data, TreeNode parent)
+        void AddChild(TreeNode Child, TreeNode parent)
         {
-            for(int i = 0; i<data.Length && i<parent.Data.Length; i++)
+            for(int i = 0; i< Child.Data.Length && i<parent.Data.Length; i++)
             {
-                if (data[i] < parent.Data[i])
+                if (Child.Data[i] < parent.Data[i])
                 {
                     if (parent.ChildrenNodes[0] != null)
                     {
-                        AddChild(data, parent.ChildrenNodes[0]);
+                        AddChild(Child, parent.ChildrenNodes[0]);
                     }
                     else
                     {
-                        TreeNode newNode = new TreeNode(data, parent);
+                        Child.ParentNode = parent;
+                        parent.ChildrenNodes[0] = Child;
                     }
                 }
-                else if (data[i] > parent.Data[i])
+                else if (Child.Data[i] > parent.Data[i])
                 {
                     if (parent.ChildrenNodes[1] != null)
                     {
-                        AddChild(data, parent.ChildrenNodes[1]);
+                        AddChild(Child, parent.ChildrenNodes[1]);
                     }
                     else
                     {
-                        TreeNode newNode = new TreeNode(data, parent);
+                        Child.ParentNode = parent;
+                        parent.ChildrenNodes[1] = Child;
                     }
                 }
             }
-            if (data.Length < parent.Data.Length)
+            if (Child.Data.Length < parent.Data.Length)
             {
                 if (parent.ChildrenNodes[0] != null)
                 {
-                    AddChild(data, parent.ChildrenNodes[0]);
+                    AddChild(Child, parent.ChildrenNodes[0]);
                 }
                 else
                 {
-                    TreeNode newNode = new TreeNode(data, parent);
+                    Child.ParentNode = parent;
+                    parent.ChildrenNodes[0] = Child;
                 }
             }
             else
             {
                 if (parent.ChildrenNodes[1] != null)
                 {
-                    AddChild(data, parent.ChildrenNodes[1]);
+                    AddChild(Child, parent.ChildrenNodes[1]);
                 }
                 else
                 {
-                    TreeNode newNode = new TreeNode(data, parent);
+                    Child.ParentNode = parent;
+                    parent.ChildrenNodes[1] = Child;
                 }
             }
             
@@ -80,7 +83,139 @@ namespace BinaryTree
 
         public void AddNode(string data)
         {
-            AddChild(data, root);
+            AddChild(new TreeNode(data, null), root);
+        }
+
+        (bool,TreeNode) SearchThroughChildren(string data, TreeNode parent)
+        {
+            for (int i = 0; i < data.Length && i < parent.Data.Length; i++)
+            {
+                if (data[i] < parent.Data[i])
+                {
+                    if (parent.ChildrenNodes[0] != null)
+                    {
+                        if (parent.ChildrenNodes[0].Data != data)
+                        {
+                            SearchThroughChildren(data, parent.ChildrenNodes[0]);
+                        }
+                        else
+                        {
+                            return (true, parent.ChildrenNodes[0]);
+                        }
+                    }
+                    else
+                    {
+                        return (false,null);
+                    }
+                }
+                else if (data[i] > parent.Data[i])
+                {
+                    if (parent.ChildrenNodes[1] != null)
+                    {
+                        if (parent.ChildrenNodes[1].Data != data)
+                        {
+                            SearchThroughChildren(data, parent.ChildrenNodes[1]);
+                        }
+                        else
+                        {
+                            return (true, parent.ChildrenNodes[1]);
+                        }
+                    }
+                    else
+                    {
+                        return (false, null);
+                    }
+                }
+            }
+            if (data.Length < parent.Data.Length)
+            {
+                if (parent.ChildrenNodes[0] != null)
+                {
+                    if (parent.ChildrenNodes[0].Data != data)
+                    {
+                        SearchThroughChildren(data, parent.ChildrenNodes[0]);
+                    }
+                    else
+                    {
+                        return (true, parent.ChildrenNodes[0]);
+                    }
+                }
+                else
+                {
+                    return (false, null);
+                }
+            }
+            else
+            {
+                if (parent.ChildrenNodes[1] != null)
+                {
+                    if (parent.ChildrenNodes[1].Data != data)
+                    {
+                        SearchThroughChildren(data, parent.ChildrenNodes[1]);
+                    }
+                    else
+                    {
+                        return (true, parent.ChildrenNodes[1]);
+                    }
+                }
+                else
+                {
+                    return (false, null);
+                }
+            }
+            return (false, null); //Code should never reach this point, but visual studio complains if it is not here
+        }
+
+        public bool Contains(string data)
+        {
+            (bool a, TreeNode b) =  SearchThroughChildren(data, root); //Don't care about the treeNode, but it is easier to do it this way
+            return a;
+        }
+
+        public void Remove(string data)
+        {
+            (bool a, TreeNode b) = SearchThroughChildren(data, root);
+            if (a) //Runs if the specified bit of data is in the tree
+            {
+                if (b.ChildrenNodes[0] != null && b.ChildrenNodes[1] != null)
+                {
+                    AddChild(b.ChildrenNodes[0], b.ParentNode);
+                    AddChild(b.ChildrenNodes[1], b.ParentNode);
+                }
+                else if (b.ChildrenNodes[0] != null)
+                {
+                    if (b.ParentNode.ChildrenNodes[0] == b)
+                    {
+                        b.ParentNode.ChildrenNodes[0] = b.ChildrenNodes[0];
+                    }
+                    else
+                    {
+                        b.ParentNode.ChildrenNodes[1] = b.ChildrenNodes[0];
+                    }
+                }
+                else if (b.ChildrenNodes[1] != null)
+                {
+                    if (b.ParentNode.ChildrenNodes[0] == b)
+                    {
+                        b.ParentNode.ChildrenNodes[0] = b.ChildrenNodes[1];
+                    }
+                    else
+                    {
+                        b.ParentNode.ChildrenNodes[1] = b.ChildrenNodes[1];
+                    }
+                }
+                else
+                {
+                    if (b.ParentNode.ChildrenNodes[0] == b)
+                    {
+                        b.ParentNode.ChildrenNodes[0] = null;
+                    }
+                    else
+                    {
+                        b.ParentNode.ChildrenNodes[1] = null;
+                    }
+                }
+            }
         }
     }
 }
